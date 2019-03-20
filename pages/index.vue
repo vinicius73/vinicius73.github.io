24d7ca6bdd49a6fa752375b27991653f4e617c0b
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-03-20 18:11:51 -0300

    🚧 index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,34 @@
 <template>
   <section class="container">
-    ~
+    <div class="columns">
+      <div class="column is-half is-offset-one-quarter">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image">
+              <img src="~/assets/profile-avatar.jpg" alt="">
+            </figure>
+          </div>
+          <div class="card-content">
+            <section class="hero">
+              <div class="hero-body">
+                <h1 class="title">
+                  Vinicius Reis
+                </h1>
+                <h2 class="subtitle">
+                  Severivo Full-Stack Developer 🤓
+                </h2>
+                <p>Married and father a beautiful daughter.</p>
+                <p>I like movies, books, manga, comics, games...</p>
+                <p>That is, I like more things than I have time to do them.</p>
+                <p>
+                  I record <a href="https://www.youtube.com/channel/UCTluPqMkm90zyw6mCde561A" target="_blank">videos</a> for <a href="https://blog.codecasts.com.br/" target="_blank">CodeCasts</a> and work on <a href="https://decision6.com/">Decision6</a> as an Application Engineer.
+                </p>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -8,3 +36,9 @@
 export default {
 }
 </script>
+
+<style scoped>
+  .hero-body {
+
+  }
+</style>
