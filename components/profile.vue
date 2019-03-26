9f7637eb482a7e1670be49fd073facf96deec457
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-03-26 10:06:38 -0300

    🎨 profile component

---
+++ components/profile.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image">
+        <img src="~/assets/profile-avatar.jpg" alt="">
+      </figure>
+    </div>
+    <div class="card-content">
+      <section class="hero is-small">
+        <div class="hero-body">
+          <h1 class="title">
+            Vinicius Reis
+          </h1>
+          <h2 class="subtitle">
+            Severino Full-Stack Developer 🤓
+          </h2>
+
+          <MiniBio />
+        </div>
+
+        <div class="hero-buttons">
+          <SocialLinks />
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import SocialLinks from './social-links'
+import MiniBio from './mini-bio'
+
+export default {
+  name: 'Profile',
+  components: { SocialLinks, MiniBio }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,39 +2,14 @@
   <section class="container">
     <div class="columns">
       <div class="column is-half is-offset-one-quarter">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image">
-              <img src="~/assets/profile-avatar.jpg" alt="">
-            </figure>
-          </div>
-          <div class="card-content">
-            <section class="hero is-small">
-              <div class="hero-body">
-                <h1 class="title">
-                  Vinicius Reis
-                </h1>
-                <h2 class="subtitle">
-                  Severino Full-Stack Developer 🤓
-                </h2>
-
-                <MiniBio />
-              </div>
-
-              <div class="hero-buttons">
-                <SocialLinks />
-              </div>
-            </section>
-          </div>
-        </div>
+        <Profile />
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import SocialLinks from '~/components/social-links'
-import MiniBio from '~/components/mini-bio'
+import Profile from '~/components/profile.vue'
 
 export default {
   name: 'Index',
@@ -42,6 +17,6 @@ export default {
     title: 'Vinicius Reis',
     titleTemplate: '%s - Severino Full-Stack Developer 🤓'
   },
-  components: { SocialLinks, MiniBio }
+  components: { Profile }
 }
 </script>
