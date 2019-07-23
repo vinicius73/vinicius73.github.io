c88432892c8403a97124d32fc8c92fed0093df26
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-07-23 19:52:44 -0300

    🔥 remove skills

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,15 @@
 <template>
   <section class="container">
     <div class="columns">
-      <div class="column is-half" :class="{ 'is-offset-one-quarter': !showSkills }">
+      <div class="column is-half is-offset-one-quarter">
         <Profile />
       </div>
-      <transition name="fade">
-        <div v-if="showSkills" class="column is-half">
-          <Skills @close="showSkills = false" />
-        </div>
-      </transition>
     </div>
   </section>
 </template>
 
 <script>
 import Profile from '~/components/profile.vue'
-import Skills from '~/components/skills.vue'
 
 export default {
   name: 'Index',
@@ -23,23 +17,6 @@ export default {
     title: 'Vinicius Reis',
     titleTemplate: '%s - Severino Full-Stack Developer 🤓'
   },
-  components: { Profile, Skills },
-  data: () => ({
-    showSkills: false
-  }),
-  mounted () {
-    setTimeout(() => {
-      this.showSkills = true
-    }, 3000)
-  }
+  components: { Profile }
 }
 </script>
-
-<style scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .2s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active em versões anteriores a 2.1.8 */ {
-  opacity: 0;
-}
-</style>
