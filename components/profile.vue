9aae9af68ea20925592e4c31edfe5fbc17970496
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-12-14 13:28:44 -0300

    🚸 glitch effect

--- components/profile.vue
+++ components/profile.vue
@@ -2,7 +2,11 @@
   <div class="card">
     <div class="card-image">
       <figure class="image">
-        <img src="~/assets/profile-avatar.jpg" alt="">
+        <div class="glitch" :style="{ 'background-image': `url(${$options.avatar})` }">
+          <img src="~/assets/profile-avatar.jpg" alt="">
+          <!-- <img src="~/assets/profile-avatar.jpg" alt=""> -->
+          <!-- <img src="~/assets/profile-avatar.jpg" alt=""> -->
+        </div>
       </figure>
     </div>
     <div class="card-content">
@@ -32,7 +36,8 @@ import MiniBio from './mini-bio'
 
 export default {
   name: 'Profile',
-  components: { SocialLinks, MiniBio }
+  components: { SocialLinks, MiniBio },
+  avatar: require('~/assets/profile-avatar.jpg')
 }
 </script>
 
